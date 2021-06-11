6b3244528f35b7fa2dbb9c4e8f54fd475feb3bc1
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-06-11 16:18:12 -0700

    finished Pokegame component, added styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     )
   }
